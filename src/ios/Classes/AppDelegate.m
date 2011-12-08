a92397c7f9678a62ae74e7090572abebb5064381
Frank Schlegel <frank.schlegel@student.hpi.uni-potsdam.de>
2011-12-08 15:18:35 +0100

    Removed the "…controller" from the names of the generated xib files.
    These are no controllers, there are views! The Xcode project templates are crap...

--- src/ios/Classes/AppDelegate.m
+++ src/ios/Classes/AppDelegate.m
@@ -23,11 +23,11 @@
     // Override point for customization after application launch.
     UIViewController *viewController1, *viewController2;
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        viewController1 = [[FirstViewController alloc] initWithNibName:@"FirstViewController_iPhone" bundle:nil];
-        viewController2 = [[SecondViewController alloc] initWithNibName:@"SecondViewController_iPhone" bundle:nil];
+        viewController1 = [[FirstViewController alloc] initWithNibName:@"FirstView_iPhone" bundle:nil];
+        viewController2 = [[SecondViewController alloc] initWithNibName:@"SecondView_iPhone" bundle:nil];
     } else {
-        viewController1 = [[FirstViewController alloc] initWithNibName:@"FirstViewController_iPad" bundle:nil];
-        viewController2 = [[SecondViewController alloc] initWithNibName:@"SecondViewController_iPad" bundle:nil];
+        viewController1 = [[FirstViewController alloc] initWithNibName:@"FirstView_iPad" bundle:nil];
+        viewController2 = [[SecondViewController alloc] initWithNibName:@"SecondView_iPad" bundle:nil];
     }
     self.tabBarController = [[UITabBarController alloc] init];
     self.tabBarController.viewControllers = [NSArray arrayWithObjects:viewController1, viewController2, nil];
