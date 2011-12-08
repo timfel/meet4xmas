b109904d5c2322fd71afc88eab87f03cd78ca1e5
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2011-12-08 19:36:30 +0100

    adapt test to new server API

--- src/server/Test.java
+++ src/server/Test.java
@@ -1,20 +1,27 @@
 //package tessi.test;
 
 import com.caucho.hessian.client.HessianProxyFactory;
-import lib.java.Appointment;
+import lib.java.*;
 
 public class Test {
-
-  public interface BasicAPI {
-    public Appointment getAppointment( int appointment_id );
-  }
-
   public static void main(String[] args) throws Exception {
     //String url = "http://172.16.18.55:4567/";
-    String url = "http://localhost:4567/";
-    System.out.println(test);
+    String url = "http://localhost:4567/1/";
     HessianProxyFactory factory = new HessianProxyFactory();
-    BasicAPI appointment = (BasicAPI) factory.create(BasicAPI.class, url);
-    System.out.println(appointment.getAppointment(0));
+    Servlet appointment = (Servlet) factory.create(Servlet.class, url);
+
+    // createAppointment
+    ResponseBody response = appointment.createAppointment("tessi@tessenow.org", 0, null, new String[]{"student@hpi.uni-potsdam.de"}, 0, "my message to all my friends");
+    if(response.success)
+      System.out.println(response.payload);
+    else
+      System.out.println(response.error);
+
+    // getAppointment
+    response = appointment.getAppointment(40);
+    if(response.success)
+      System.out.println(response.payload);
+    else
+      System.out.println(response.error);
   }
 }
