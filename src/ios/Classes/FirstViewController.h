b9d2d7fccd025bc640c32b5886c0c181b3089cf8
Frank Schlegel <frank.schlegel@student.hpi.uni-potsdam.de>
2011-12-08 14:48:21 +0100

    Created stub iOS project (tab view with 2 subviews) and ignored all the user-specific files in the project.

---
+++ src/ios/Classes/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Meet4Xmas
+//
+//  Created by Frank Schlegel on 08.12.11.
+//  Copyright (c) 2011 HPI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
