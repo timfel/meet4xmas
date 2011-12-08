106dffb5477653ac5a220071172d1884ddc0d46a
Lysann Kessler <lysann.kessler@student.hpi.uni-potsdam.de>
2011-12-08 18:55:09 +0100

    transform Location and TravelPlan interfaces to classes

--- src/server/lib/java/TravelPlan.java
+++ src/server/lib/java/TravelPlan.java
@@ -1,7 +1,21 @@
 package lib.java;
 
-public interface TravelPlan {
-  public int appointmentId();
-  public Location[] path();
-  public int travelType(); // 0 = car, 1 = by foot, 2 = public transportation
+public class TravelPlan implements java.io.Serializable {
+  public int appointmentId;
+  public Location[] path;
+  public int travelType; // 0 = car, 1 = by foot, 2 = public transportation
+
+  public String toString() {
+  	StringBuilder sb = new StringBuilder("TravelPlan(");
+  	sb.append("@appointmentId: ").append(appointmentId).append(", ");
+  	sb.append("@path: [");
+  	for(int i = 0; i < path.length; i++) {
+	  	sb.append(path[i]);
+	  	if(i < path.length - 1) sb.append(", ");
+	  }
+  	sb.append("], ");
+  	sb.append("@travelType: ").append(travelType);
+  	sb.append(")");
+  	return sb.toString();
+  }
 }
