be818dc366b3e78c8911f79878f84ce43e20f244
Lysann Kessler <lysann.kessler@student.hpi.uni-potsdam.de>
2011-12-16 00:09:23 +0100

    getAppointment spec: some checks concerning returned appointment attributes

--- src/server/spec/external/get_appointment_spec.rb
+++ src/server/spec/external/get_appointment_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../service_spec_helper', __FILE__)
+require 'set'
 
 describe 'Meet4Xmas Service' do
   describe '#getAppointment' do
@@ -8,18 +9,21 @@ describe 'Meet4Xmas Service' do
     
     before :each do
       # create an appointment
-      creator = 'lysann.kessler@gmail.com'
+      @creator = 'lysann.kessler@gmail.com'
       invitees = [ 'test@example.com', 'foo@example.com' ]
       location = { 'longitude' => 0, 'latitude' => 0, 'title' => 'title', 'description' => 'description' }
+      @location_type = Meet4Xmas::Persistence::LocationType::ALL.first
+      @user_message = 'user message'
       create_appointment_args = [
-        creator, Meet4Xmas::Persistence::TravelType::ALL.first, location,
+        @creator, Meet4Xmas::Persistence::TravelType::ALL.first, location,
         invitees,
-        Meet4Xmas::Persistence::LocationType::ALL.first,
-        'user message'
+        @location_type,
+        @user_message
       ]
-      register_users creator
+      register_users @creator
       register_users *invitees
       @appointment_id = @client.createAppointment(*create_appointment_args)['payload']
+      @participants = [] + invitees; @participants << @creator
     end
 
     it 'succeeds if the appointment exists' do
@@ -30,6 +34,42 @@ describe 'Meet4Xmas Service' do
       response = @client.getAppointment(@appointment_id+1)
       response.should_not be_successful
       # TODO: test error code
+    end
+
+    it 'returns the requested appointment' do
+      payload = @client.getAppointment(@appointment_id)['payload']
+      payload['identifier'].should == @appointment_id
+    end
+
+    describe 'returns' do
+      before :each do
+        @payload = @client.getAppointment(@appointment_id)['payload']
+      end
+
+      it 'the requested appointment' do
+        @payload['identifier'].should == @appointment_id
+      end
+
+      it 'all attributes' do # TODO: split this test into subtests
+        @payload['creator'].should == @creator
+        @payload['locationType'].should == @location_type
+        #@payload['location'].should_not be_nil # TODO
+        @payload['isFinal'].should == false
+        @payload['message'].should == @user_message
+
+        returned_participants = @payload['participants']
+        returned_participants.should be_kind_of(Array)
+        returned_participants.count.should == @participants.count
+        returned_participant_ids = returned_participants.map { |p| p['userId'] }
+        Set.new(returned_participant_ids).should == Set.new(@participants)
+
+        # the following two checks are actually more like lifecycle checks...
+        creator_participant = returned_participants.find { |p| p['userId'] == @creator }
+        creator_participant['status'].should == Meet4Xmas::Persistence::ParticipationStatus::Accepted
+
+        invited_participants = returned_participants.select { |p| p['userId'] != @creator }
+        invited_participants.each { |p| p['status'].should == Meet4Xmas::Persistence::ParticipationStatus::Pending }
+      end
     end
   end
 end
