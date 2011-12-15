10fb7c2f909a63bc5e79b9f7f7704a5b699e295f
Lysann Kessler <lysann.kessler@student.hpi.uni-potsdam.de>
2011-12-15 16:10:34 +0100

    refactor createAppointment tests
    less explicit calls to register_creator and register_invitees

--- src/server/spec/external/create_appointment_spec.rb
+++ src/server/spec/external/create_appointment_spec.rb
@@ -2,15 +2,11 @@ require File.expand_path('../service_spec_helper', __FILE__)
 
 describe 'Meet4Xmas Service' do
   describe '#deleteAccount' do
-    before :each do
-      @user_id = 'lysann.kessler@gmail.com'
+    def register_users(*users)
+      users.each { |user| @client.registerAccount(user) }
     end
-    
-    def register_invitees
-      @invitees.each { |invitee| @client.registerAccount(invitee) }
-    end
-    def register_creator
-      @client.registerAccount(@creator)
+    def unregister_users(*users)
+      users.each { |user| @client.deleteAccount(user) }
     end
 
     before :each do
@@ -23,58 +19,50 @@ describe 'Meet4Xmas Service' do
         Meet4Xmas::Persistence::LocationType::ALL.first,
         'user message'
       ]
+
+      register_users @creator
+      register_users *@invitees
     end
 
     it 'succeeds if everything is alright' do
-      register_creator
-      register_invitees
       response = @client.createAppointment(*@create_appointment_args)
       response.should be_successful
     end
 
     it 'returns the appointment id' do
-      register_creator
-      register_invitees
       response = @client.createAppointment(*@create_appointment_args)
       payload = response['payload']
       payload.should be_kind_of(Numeric)
     end
 
     it 'can create multiple appointments' do
-      register_creator
-      register_invitees
       response = @client.createAppointment(*@create_appointment_args)
       response.should be_successful
       response = @client.createAppointment(*@create_appointment_args)
       response.should be_successful
     end
 
-    it 'returns subsequent appointment ids' do
-      register_creator
-      register_invitees
+    it 'returns different appointment ids for different appointments' do
       id1 = @client.createAppointment(*@create_appointment_args)['payload']
       id2 = @client.createAppointment(*@create_appointment_args)['payload']
-      id2.should == (id1+1)
+      id1.should_not == id2
     end
 
     it 'fails if the creator is not registered' do
-      register_invitees
+      unregister_users @creator
       response = @client.createAppointment(*@create_appointment_args)
       response.should_not be_successful
       # TODO: test error code
     end
 
-    it 'fails if one invitee is not registered' do
-      register_creator
-      @invitees.take(@invitees.size - 1).each { |invitee| @client.registerAccount(invitee) }
+    it 'fails if at least one invitee is not registered' do
+      unregister_users @invitees.first
       response = @client.createAppointment(*@create_appointment_args)
       response.should_not be_successful
       # TODO: test error code
     end
 
     it 'fails if the creator id is missing' do
-      register_creator
-      register_invitees
       @create_appointment_args[0] = nil
       response = @client.createAppointment(*@create_appointment_args)
       response.should_not be_successful
@@ -82,8 +70,6 @@ describe 'Meet4Xmas Service' do
     end
 
     it 'fails if the travel type is invalid' do
-      register_creator
-      register_invitees
       @create_appointment_args[1] = Meet4Xmas::Persistence::TravelType::ALL.reduce(:+) + 1
       response = @client.createAppointment(*@create_appointment_args)
       response.should_not be_successful
@@ -91,8 +77,6 @@ describe 'Meet4Xmas Service' do
     end
 
     it 'fails if the location is missing' do
-      register_creator
-      register_invitees
       @create_appointment_args[2] = nil
       response = @client.createAppointment(*@create_appointment_args)
       response.should_not be_successful
@@ -100,15 +84,13 @@ describe 'Meet4Xmas Service' do
     end
 
     it 'succeeds if there are no invitees' do
-      register_creator
+      unregister_users *@invitees # optional for this test
       @create_appointment_args[3] = []
       response = @client.createAppointment(*@create_appointment_args)
       response.should be_successful
     end
 
     it 'fails if the location type is invalid' do
-      register_creator
-      register_invitees
       @create_appointment_args[4] = Meet4Xmas::Persistence::LocationType::ALL.reduce(:+) + 1
       response = @client.createAppointment(*@create_appointment_args)
       response.should_not be_successful
