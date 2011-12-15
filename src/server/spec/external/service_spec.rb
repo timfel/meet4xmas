0109e0e95eaf4fac26389dfa812afc02331bdf78
Lysann Kessler <lysann.kessler@student.hpi.uni-potsdam.de>
2011-12-15 11:15:00 +0100

    clean up DB before each test

--- src/server/spec/external/service_spec.rb
+++ src/server/spec/external/service_spec.rb
@@ -3,6 +3,9 @@ require File.expand_path('../../../lib/server/api', __FILE__)
 
 require File.expand_path('../../../lib/hessian_client/lib/hessian', __FILE__)
 
+require 'rubygems'
+require 'dm-core'
+
 MEET4XMAS_TEST_PORT = 4568
 
 describe 'Meet4Xmas Service' do
@@ -22,6 +25,11 @@ describe 'Meet4Xmas Service' do
     # initialize client
     @_address = "http://localhost:#{MEET4XMAS_TEST_PORT}/#{Meet4Xmas::Server::API::VERSION}/"
     @client = Hessian::HessianClient.new(@_address)
+  end
+
+  # clean up database before each test
+  before :each do
+    DataMapper.auto_migrate!
   end
 
   describe '#registerAccount' do
