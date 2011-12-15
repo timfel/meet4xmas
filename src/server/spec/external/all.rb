1d40939ab308a83e80c3024a26dbdeefd0f038f3
Lysann Kessler <lysann.kessler@student.hpi.uni-potsdam.de>
2011-12-15 15:54:21 +0100

    adding tests for createAppointment

--- src/server/spec/external/all.rb
+++ src/server/spec/external/all.rb
@@ -1,2 +1,3 @@
 require File.expand_path('../register_account_spec', __FILE__)
 require File.expand_path('../delete_account_spec', __FILE__)
+require File.expand_path('../create_appointment_spec', __FILE__)
