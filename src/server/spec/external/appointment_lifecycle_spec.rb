f5b877a2a7e624df267cab68ee1c289e4df0e436
Lysann Kessler <lysann.kessler@student.hpi.uni-potsdam.de>
2011-12-16 15:14:54 +0100

    extend lifecycle tests

--- src/server/spec/external/appointment_lifecycle_spec.rb
+++ src/server/spec/external/appointment_lifecycle_spec.rb
@@ -8,26 +8,36 @@ describe 'Meet4Xmas Service' do
 
     def create_appointment
       @creator = 'lysann.kessler@gmail.com'
-      invitees = [ 'test@example.com', 'foo@example.com' ]
-      @participants = [] + invitees; @participants << @creator
-      location = { 'longitude' => 0, 'latitude' => 0, 'title' => 'title', 'description' => 'description' }
+      @invitees = [ 'test@example.com', 'foo@example.com' ]
+      @participants = [] + @invitees; @participants << @creator
+      location = { 'longitude' => 0, 'latitude' => 0 }
       @location_type = Meet4Xmas::Persistence::LocationType::ALL.first
       @user_message = 'user message'
       
       create_appointment_args = [
         @creator, Meet4Xmas::Persistence::TravelType::ALL.first, location,
-        invitees,
+        @invitees,
         @location_type,
         @user_message
       ]
       
       register_users @creator
-      register_users *invitees
+      register_users(*@invitees)
       @appointment_id = @client.createAppointment(*create_appointment_args)['payload']
     end
 
     def get_appointment(id)
       @client.getAppointment(id)
+    end
+
+    def try_join
+      location = { 'longitude' => 1, 'latitude' => 0 }
+      travel_type = Meet4Xmas::Persistence::TravelType::ALL.first
+      response = @client.joinAppointment(@appointment_id, @invitees.first, travel_type, location)
+    end
+
+    def try_decline
+      response = @client.declineAppointment(@appointment_id, @invitees.first)
     end
 
     it 'In the beginning there is no appointment' do
@@ -54,6 +64,38 @@ describe 'Meet4Xmas Service' do
         returned_participants = @appointment['participants']
         invited_participants = returned_participants.select { |p| p['userId'] != @creator }
         invited_participants.each { |p| p['status'].should == Meet4Xmas::Persistence::ParticipationStatus::Pending }
+      end
+
+      it 'invitees can join' do
+        try_join.should be_successful
+      end
+
+      it 'invitees can decline' do
+        try_decline.should be_successful
+      end
+    end
+
+    describe 'After finalization' do
+      before :each do
+        id = create_appointment
+        @client.finalizeAppointment(id)
+        @appointment = get_appointment(id)['payload']
+      end
+
+      it 'the appointment is final' do
+        @appointment['isFinal'].should == true
+      end
+
+      it 'travel plans do not change anymore' do
+        pending
+      end
+
+      it 'joining still succeeds' do
+        try_join.should be_successful
+      end
+
+      it 'declining still succeeds' do
+        try_decline.should be_successful
       end
     end
   end
