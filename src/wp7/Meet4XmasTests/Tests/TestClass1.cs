9a61ede45c16660e20a6a51de4d70cf19f873784
Tim Felgentreff <timfelgentreff@gmail.com>
2011-12-16 16:03:35 +0100

    Add tests

--- src/wp7/Meet4XmasTests/Tests/TestClass1.cs
+++ src/wp7/Meet4XmasTests/Tests/TestClass1.cs
@@ -14,31 +14,83 @@ namespace Meet4XmasTests.Tests
         const string url = "http://tessi.fornax.uberspace.de/xmas/1/";
         //const string url = "http://172.16.18.83:4567/1/";
 
-        object global;
-        CHessianProxyFactory factory;
-        CAsyncHessianMethodCaller methodCaller;
-
-        [TestInitialize]
-        public void setUp()
+        [TestMethod]
+        [Asynchronous]
+        public void TestAccCreate()
         {
-            global = null;
-            factory = new CHessianProxyFactory();
-            methodCaller = new CAsyncHessianMethodCaller(factory, new Uri(url));
+            object result = null;
+            string name = "Tim" + DateTime.Now.ToString();
+            EnqueueCallback(() => Account.Create(name,
+                                                (Account ac) => result = ac,
+                                                (ErrorInfo ei) => result = ei));
+            EnqueueConditional(() => result != null);
+            EnqueueCallback(() =>
+            {
+                Assert.IsInstanceOfType(result, typeof(Account));
+                Assert.AreEqual(((Account)result).userId, name);
+            });
+            EnqueueTestComplete();
         }
 
         [TestMethod]
         [Asynchronous]
-        public void TestMethod1()
+        public void TestAccDelete()
         {
-            string name = "Tim" + DateTime.Now.ToString();
+            string name = "Tim";
+            Account tim = null;
+            ErrorInfo error = null;
             EnqueueCallback(() => Account.Create(name,
-                                                (Account ac) => global = ac,
-                                                (ErrorInfo ei) => global = ei));
-            EnqueueConditional(() => global != null);
+                                                (Account ac) => tim = ac,
+                                                (ErrorInfo ei) => error = ei));
+            EnqueueConditional(() => tim != null || error != null);
             EnqueueCallback(() =>
             {
-                Assert.IsInstanceOfType(global, typeof(Account));
-                Assert.AreEqual(((Account)global).userId, name);
+                Assert.IsNotNull(tim);
+                Assert.IsNull(error);
+                tim.Delete(() => tim = null,
+                           (ErrorInfo ei) => error = ei);
+            });
+            EnqueueConditional(() => tim == null || error != null);
+            EnqueueCallback(() =>
+            {
+                Assert.IsNull(error);
+                Assert.IsNull(tim);
+            });
+            EnqueueTestComplete();
+        }
+
+        [TestMethod]
+        [Asynchronous]
+        public void TestAppointmentCreate()
+        {
+            string name = "Tim";
+            Account tim = null;
+            ErrorInfo error = null;
+            object app = null;
+            string testMessage = "Test Appointment";
+
+            EnqueueCallback(() => Account.Create(name,
+                                                (Account ac) => tim = ac,
+                                                (ErrorInfo ei) => error = ei));
+            EnqueueConditional(() => tim != null || error != null);
+            EnqueueCallback(() =>
+            {
+                Assert.IsNotNull(tim);
+                Appointment.Create(tim, TravelPlan.TravelType.Walk, new Participant[] { tim },
+                    Location.LocationType.ChristmasMarket, testMessage,
+                    (Appointment a) => app = a,
+                    (ErrorInfo e) => error = e);
+            });
+            EnqueueConditional(() => app != null || error != null);
+            EnqueueCallback(() =>
+            {
+                Assert.IsNull(error);
+                Assert.IsInstanceOfType(app, typeof(Appointment));
+                Assert.AreEqual(((Appointment)app).creator, tim.userId);
+                Assert.IsNotNull(((Appointment)app).identifier);
+                Assert.AreEqual(((Appointment)app).message, testMessage);
+                Assert.AreEqual(((Appointment)app).locationType, Location.LocationType.ChristmasMarket);
+                Assert.IsFalse(((Appointment)app).isFinal);
             });
             EnqueueTestComplete();
         }
