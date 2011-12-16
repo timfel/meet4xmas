5d2830bc6c5c45c2935f0f36cf7cbf8e27700ded
Lysann Kessler <lysann.kessler@student.hpi.uni-potsdam.de>
2011-12-16 14:54:43 +0100

    fix coverage report built by rcov
    rspec needs to be executed explicitly for some reason… (see all.rb)

--- src/server/spec/external/all.rb
+++ src/server/spec/external/all.rb
@@ -1,5 +1,9 @@
+require 'rspec/core'
+
 require File.expand_path('../register_account_spec', __FILE__)
 require File.expand_path('../delete_account_spec', __FILE__)
 require File.expand_path('../create_appointment_spec', __FILE__)
 require File.expand_path('../get_appointment_spec', __FILE__)
 require File.expand_path('../appointment_lifecycle_spec', __FILE__)
+
+RSpec::Core::Runner.run([])
