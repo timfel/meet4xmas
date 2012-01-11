b01589224804e3ee40863fdcb90d2d7fbc09fa90
Lysann Kessler <lysann.kessler@student.hpi.uni-potsdam.de>
2012-01-11 15:28:41 +0100

    refactor create_appointment_spec

--- src/server/spec/external/create_appointment_spec.rb
+++ src/server/spec/external/create_appointment_spec.rb
@@ -2,100 +2,83 @@ require File.expand_path('../service_spec_helper', __FILE__)
 
 describe 'Meet4Xmas Service' do
   describe '#deleteAccount' do
-    def register_users(*users)
-      users.each { |user| @client.registerAccount(user) }
-    end
-    def unregister_users(*users)
-      users.each { |user| @client.deleteAccount(user) }
-    end
-
     before :each do
-      @creator = 'lysann.kessler@gmail.com'
-      @invitees = [ 'test@example.com', 'foo@example.com' ]
-      @location = Meet4Xmas::Persistence::Location.HPI
-      @create_appointment_args = [
-        @creator, Meet4Xmas::Persistence::TravelType::ALL.first, @location.to_hash,
-        @invitees,
-        Meet4Xmas::Persistence::LocationType::ALL.first,
-        'user message'
-      ]
-
-      register_users @creator
-      register_users(*@invitees)
+      create_appointment_args # load them so we can manipulate them later
+      register_all
     end
 
     it 'succeeds if everything is alright' do
-      @client.createAppointment(*@create_appointment_args).should be_successful
+      create_appointment.should be_successful
     end
 
     it 'returns the appointment id' do
-      payload = @client.createAppointment(*@create_appointment_args)['payload']
+      payload = create_appointment['payload']
       payload.should be_kind_of(Numeric)
     end
 
     it 'can create multiple appointments' do
-      @client.createAppointment(*@create_appointment_args).should be_successful
-      @client.createAppointment(*@create_appointment_args).should be_successful
+      create_appointment.should be_successful
+      create_appointment.should be_successful
     end
 
     it 'returns different appointment ids for different appointments' do
-      id1 = @client.createAppointment(*@create_appointment_args)['payload']
-      id2 = @client.createAppointment(*@create_appointment_args)['payload']
+      id1 = create_appointment['payload']
+      id2 = create_appointment['payload']
       id1.should_not == id2
     end
 
     it 'fails if the creator is not registered' do
       unregister_users @creator
-      response = @client.createAppointment(*@create_appointment_args)
+      response = create_appointment
       response.should_not be_successful
       # TODO: test error code
     end
 
-    it 'fails if at least one invitee is not registered' do
+    it 'succeeds even if an invitee is not registered' do
       unregister_users @invitees.first
-      response = @client.createAppointment(*@create_appointment_args)
-      response.should_not be_successful
+      response = create_appointment
+      response.should be_successful
       # TODO: test error code
     end
 
     it 'fails if the creator id is missing' do
-      @create_appointment_args[0] = nil
-      response = @client.createAppointment(*@create_appointment_args)
+      create_appointment_args[0] = nil
+      response = create_appointment
       response.should_not be_successful
       # TODO: test error code
     end
 
     it 'fails if the travel type is invalid' do
-      @create_appointment_args[1] = Meet4Xmas::Persistence::TravelType::ALL.reduce(:+) + 1
-      response = @client.createAppointment(*@create_appointment_args)
+      create_appointment_args[1] = Meet4Xmas::Persistence::TravelType::ALL.reduce(:+) + 1
+      response = create_appointment
       response.should_not be_successful
       # TODO: test error code
     end
 
     it 'fails if the location is missing' do
-      @create_appointment_args[2] = nil
-      response = @client.createAppointment(*@create_appointment_args)
+      create_appointment_args[2] = nil
+      response = create_appointment
       response.should_not be_successful
       # TODO: test error code
     end
 
     it 'succeeds if there are no invitees' do
       unregister_users(*@invitees) # optional for this test
-      @create_appointment_args[3] = []
-      response = @client.createAppointment(*@create_appointment_args)
+      create_appointment_args[3] = []
+      response = create_appointment
       response.should be_successful
     end
 
     it 'fails if the location type is invalid' do
-      @create_appointment_args[4] = Meet4Xmas::Persistence::LocationType::ALL.reduce(:+) + 1
-      response = @client.createAppointment(*@create_appointment_args)
+      create_appointment_args[4] = Meet4Xmas::Persistence::LocationType::ALL.reduce(:+) + 1
+      response = create_appointment
       response.should_not be_successful
       # TODO: test error code
     end
 
     it 'succeeds if the user message is missing' do
-      @create_appointment_args[5] = nil
-      response = @client.createAppointment(*@create_appointment_args)
+      create_appointment_args[5] = nil
+      response = create_appointment
       response.should be_successful
     end
 
@@ -105,16 +88,16 @@ describe 'Meet4Xmas Service' do
       }.each do |key, values|
         values['valid_values'].each do |value|
           it "succeeds if location #{key} is valid (#{value})" do
-            location = @create_appointment_args[2]
+            location = create_appointment_args[2]
             location[key] = value
-            @client.createAppointment(*@create_appointment_args).should be_successful
+            create_appointment.should be_successful
           end
         end
         values['invalid_values'].each do |value|
           it "fails if location #{key} is invalid (#{value})" do
-            location = @create_appointment_args[2]
+            location = create_appointment_args[2]
             location[key] = value
-            response = @client.createAppointment(*@create_appointment_args)
+            response = create_appointment
             response.should_not be_successful
             # TODO: test error code
           end
@@ -122,10 +105,10 @@ describe 'Meet4Xmas Service' do
       end
 
       it 'succeeds if location title and description are missing' do
-        location = @create_appointment_args[2]
+        location = create_appointment_args[2]
         location.delete('title')
         location.delete('description')
-        @client.createAppointment(*@create_appointment_args).should be_successful
+        create_appointment.should be_successful
       end
     end
   end
--- src/server/spec/external/service_spec_helper.rb
+++ src/server/spec/external/service_spec_helper.rb
@@ -22,6 +22,66 @@ require File.expand_path('../../../lib/server/api', __FILE__)
 require File.expand_path('../../../lib/hessian_client/lib/hessian', __FILE__)
 
 #
+# helpers
+#
+
+module Helpers
+  # server setup & connection
+
+  def _start_server(port =4568)
+    @_port = port
+    @_server = Meet4Xmas::Server::run @_port
+  end
+
+  def _stop_server
+    @_server.stop
+  end
+
+  def _connect
+    @_address = "http://localhost:#{@_port}/#{Meet4Xmas::Server::API::VERSION}/"
+    @client = Hessian::HessianClient.new(@_address)
+  end
+
+  def _cleanup_db
+    DataMapper.auto_migrate!
+  end
+
+  # user registration
+
+  def register_users(*users)
+    users.each { |user| @client.registerAccount(user) }
+  end
+
+  def unregister_users(*users)
+    users.each { |user| @client.deleteAccount(user) }
+  end
+
+  def register_all
+    register_users @creator if defined?(@creator)
+    register_users(*@invitees) if defined?(@invitees)
+  end
+
+  # appointment creation
+
+  def create_appointment_args
+    @creator ||= 'lysann.kessler@gmail.com'
+    @invitees ||= [ 'test@example.com', 'foo@example.com' ]
+    @location ||= Meet4Xmas::Persistence::Location.HPI
+    @create_appointment_args ||= [
+      @creator, Meet4Xmas::Persistence::TravelType::ALL.first, @location.to_hash,
+      @invitees,
+      Meet4Xmas::Persistence::LocationType::ALL.first,
+      'user message'
+    ]
+  end
+
+  def create_appointment(*args)
+    args = create_appointment_args if args.empty?
+    @client.createAppointment(*args)
+  end
+end
+
+#
 # custom matchers for response objects
 #
 
@@ -59,26 +119,24 @@ require 'dm-core'
 
 require 'rspec/core'
 RSpec.configure do |config|
+  config.include Helpers
+  config.extend Helpers
+
   # server setup & teardown
   config.before :all do
-    # start server
-    @_port = 4568
-    @_server = Meet4Xmas::Server::run @_port
+    _start_server
   end
   config.after :all do
-    # stop server
-    @_server.stop
+    _stop_server
   end
 
   # client setup
   config.before :all do
-    # initialize client
-    @_address = "http://localhost:#{@_port}/#{Meet4Xmas::Server::API::VERSION}/"
-    @client = Hessian::HessianClient.new(@_address)
+    _connect
   end
 
   # clean up database before each test
   config.before(:each) do
-    DataMapper.auto_migrate!
+    _cleanup_db
   end
 end
