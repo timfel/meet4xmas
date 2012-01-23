e068e6deb17590cc4bdc03b065e3935d9cacf6bd
Robert Aschenbrenner <trobbe@online.de>
2012-01-23 19:00:11 +0100

    includes example calls in signupactivity

--- src/android/src/de/uni_potsdam/hpi/meet4android/SignUpActivity.java
+++ src/android/src/de/uni_potsdam/hpi/meet4android/SignUpActivity.java
@@ -1,12 +1,18 @@
 package de.uni_potsdam.hpi.meet4android;
 
+import java.net.MalformedURLException;
+
+import org.meet4xmas.wire.IServiceAPI;
+import org.meet4xmas.wire.Response;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.caucho.hessian.client.HessianProxyFactory;
 
 public class SignUpActivity extends Activity {
     /** Called when the activity is first created. */
@@ -16,6 +22,47 @@ public class SignUpActivity extends Activity {
         setContentView(R.layout.main);
 
         setupSignUpButton();
+
+        String url = "http://tessi.fornax.uberspace.de/xmas/2/";
+        HessianProxyFactory factory = new HessianProxyFactory();
+        // Use Hessian protocol v1
+        factory.setHessian2Request(false);
+        factory.setHessian2Reply(false);
+
+        IServiceAPI proxy;
+        Response response;
+		try {
+			proxy = (IServiceAPI) factory.create(IServiceAPI.class, url);
+
+//			NotificationServiceInfo notificationServiceInfo = new NotificationServiceInfo();
+//			notificationServiceInfo.serviceType = 2;
+//			notificationServiceInfo.deviceId = new byte[]{0,0,7};
+
+			response = proxy.registerAccount("robert.aschenbrenner@student.hpi.uni-potsdam.de", null);
+	        if(response.success)
+	          System.out.println(response.payload);
+	        else
+	          System.out.println(response.error);
+
+	        // createAppointment
+//			Location loc = new Location();
+//			loc.latitude = 52.440107;
+//			loc.longitude = 13.246536;
+//	        response = proxy.createAppointment("robert.aschenbrenner@student.hpi.uni-potsdam.de", 0, loc, new String[]{"lysann.kessler@gmail.com"}, 0, "my message to all my friends");
+//	        if(response.success)
+//	          System.out.println(response.payload);
+//	        else
+//	          System.out.println(response.error);
+
+	        // getAppointment
+	        response = proxy.getAppointment(3);
+	        if(response.success)
+	          System.out.println(response.payload);
+	        else
+	          System.out.println(response.error);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
     }
 
     protected void setupSignUpButton() {
