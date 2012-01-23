15659ca2bd378d89dafa4e6c07b9dfebfc2a5baf
Markus Kahl <machisuji@googlemail.com>
2012-01-23 17:58:35 +0100

    first view: SignUp activity (all looks)

--- src/android/src/de/uni_potsdam/hpi/meet4android/SignUpActivity.java
+++ src/android/src/de/uni_potsdam/hpi/meet4android/SignUpActivity.java
@@ -3,7 +3,7 @@ package de.uni_potsdam.hpi.meet4android;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Meet4androidActivity extends Activity {
+public class SignUpActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
