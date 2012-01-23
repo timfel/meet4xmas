a79832c3402bdb1f6422db1d6d8261784ac51e6b
Markus Kahl <machisuji@googlemail.com>
2012-01-23 18:48:00 +0100

    wow clicking a button !!!

--- src/android/src/de/uni_potsdam/hpi/meet4android/SignUpActivity.java
+++ src/android/src/de/uni_potsdam/hpi/meet4android/SignUpActivity.java
@@ -2,6 +2,11 @@ package de.uni_potsdam.hpi.meet4android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUpActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +14,17 @@ public class SignUpActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        setupSignUpButton();
+    }
+
+    protected void setupSignUpButton() {
+        final Button button = (Button) findViewById(R.id.signUpButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String email = ((TextView) findViewById(R.id.signUpEmail)).getText().toString();
+                Toast.makeText(SignUpActivity.this, "Given email: " + email, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
