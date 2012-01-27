1c19abef812711e0c4c997751d0f16364643928b
Tim Felgentreff <timfelgentreff@gmail.com>
2012-01-27 10:15:32 +0100

    add settings page

---
+++ src/wp7/Meet4Xmas/SettingsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using org.meet4xmas.wire;
+using System.Windows.Controls.Primitives;
+using Microsoft.Phone.Tasks;
+using System.Linq;
+using Microsoft.Phone.Controls.Maps;
+using System.Device.Location;
+
+
+namespace Meet4Xmas
+{
+    public partial class SettingsPage : PhoneApplicationPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+            InitializeTravelTypes();
+        }
+
+        private void InitializeTravelTypes()
+        {
+            listPicker.ItemsSource = new List<string>(TravelPlan.TravelType.TypesList);
+            listPicker.SelectedIndex = Settings.PreferredTravelType;
+            listPicker.SelectionChanged += new SelectionChangedEventHandler(listPicker_SelectionChanged);
+        }
+
+        private void listPicker_SelectionChanged(object sender, EventArgs e)
+        {
+            Settings.PreferredTravelType = listPicker.SelectedIndex;
+        }
+    }
+}
