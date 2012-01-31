0c3a7f36311d412de71737616e2c884b960f51dc
Markus Kahl <machisuji@googlemail.com>
2012-01-31 15:26:27 +0100

    display C2DM registration updates (we can now process push notifications)

--- src/android/src/de/uni_potsdam/hpi/meet4android/C2DMReceiver.java
+++ src/android/src/de/uni_potsdam/hpi/meet4android/C2DMReceiver.java
@@ -1,12 +1,23 @@
 package de.uni_potsdam.hpi.meet4android;
 
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class C2DMReceiver extends BroadcastReceiver {
 
+    public C2DMReceiver() {
+        Log.d("xmas", "instantiated C2DMReceiver");
+    }
+
     public void onReceive(Context context, Intent intent) {
+        Log.d("xmas", "receiving something");
         if (intent.getAction().equals("com.google.android.c2dm.intent.REGISTRATION")) {
             handleRegistration(context, intent);
         } else if (intent.getAction().equals("com.google.android.c2dm.intent.RECEIVE")) {
@@ -16,18 +27,35 @@ public class C2DMReceiver extends BroadcastReceiver {
 
     public void handleRegistration(Context context, Intent intent) {
         String registration = intent.getStringExtra("registration_id");
+
         if (intent.getStringExtra("error") != null) {
-            // Registration failed, should try again later.
+            int id = context.getResources().getIdentifier("error_" + intent.getStringExtra("error").toLowerCase(),
+                    "string", "de.uni_potsdam.hpi.meet4android");
+            Toast.makeText(context, id, Toast.LENGTH_LONG).show();
         } else if (intent.getStringExtra("unregistered") != null) {
-            // unregistration done, new messages from the authorized sender will be rejected
+            Toast.makeText(context, "Unregistered", Toast.LENGTH_LONG).show();
         } else if (registration != null) {
-            // Send the registration ID to the 3rd party site that is sending the messages.
-            // This should be done in a separate thread.
-            // When done, remember that all registration is done.
+            new Preferences(context).setRegistrationId(registration);
+            synchronizeRegistrationId(registration);
+            Toast.makeText(context, "Registered", Toast.LENGTH_LONG).show();
         }
     }
 
     public void handleMessage(Context context, Intent intent) {
+        Log.d("xmas", "receiving message");
         String foobar = intent.getExtras().getString("foobar");
+    }
+
+    /**
+     * Sends the received registration_id to the meet4xmas server in order to get push notifications from it.
+     *
+     * @param id The registration id as received from Google.
+     */
+    public void synchronizeRegistrationId(String id) {
+        new Thread(new Runnable() {
+            public void run() {
+                // @TODO send it to the server
+            }
+        }).start();
     }
 }
